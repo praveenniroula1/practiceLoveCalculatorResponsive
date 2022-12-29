49082a5d8f0c44151dc4da92e3e5ce7b4fdb2d6c
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2022-12-30 10:00:48 +1100

    updated version to display name of input feild

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
 const button = document.getElementById("button");
-let percent = "";
 button.addEventListener("click", () => {
   const data = Math.random() * 100;
   const realData = Math.floor(data);
   const percent = realData;
   if (percent >= 50) {
-    display.innerHTML = "Congratulation" + " " + percent + "%.";
+    display.innerHTML =
+      "Congratulation!!!" +
+      " " +
+      yourNameValue +
+      " " +
+      "and" +
+      " " +
+      partnerNameValue +
+      " " +
+      "have compatibility of" +
+      " " +
+      percent +
+      "%.";
   } else {
-    display.innerHTML = "Oopsss" + " " + percent + "%" + "only.";
+    display.innerHTML =
+      "Ooopsss!!!" +
+      " " +
+      yourNameValue +
+      " " +
+      "and" +
+      " " +
+      partnerNameValue +
+      " " +
+      "have compatibility of only" +
+      " " +
+      percent +
+      "%.";
   }
 });
 
 const display = document.getElementById("display");
+
+const yourName = document.getElementById("yourName");
+const partnerName = document.getElementById("partnerName");
+let yourNameValue = "";
+let partnerNameValue = "";
+
+const nameDisplay = () => {
+  yourName.addEventListener("change", () => {
+    yourNameValue = yourName.value;
+  });
+  partnerName.addEventListener("change", () => {
+    partnerNameValue = partnerName.value;
+  });
+};
+console.log(nameDisplay());
+nameDisplay();
